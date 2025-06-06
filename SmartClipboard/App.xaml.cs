88d73a146635f4bc3d13fe5bb74448b34a2c090a
IvanAfanasiev <afanasievivan10@gmail.com>
2025-06-06 13:52:19 +0200

    Initialize SmartClipboard project

---
+++ SmartClipboard/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace SmartClipboard
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
