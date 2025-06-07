eed6bc57b531876ab799b7ae16850c93a130d367
IvanAfanasiev <afanasievivan10@gmail.com>
2025-06-07 13:36:51 +0200

    Add clipboard monitoring(WinAPI hook), SQLite storage and basic WPF UI

---
+++ SmartClipboard/Models/ClipboardItem.cs
@@ -0,0 +1,27 @@
+﻿using SmartClipboard.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartClipboard.Models
+{
+    internal class ClipboardItem
+    {
+        public long Id { get; set; }
+        public string Content { get; set; } = string.Empty;
+        public ContentType Type { get; set; } = ContentType.Text;
+        public DateTime Timestamp { get; set; } = DateTime.Now;
+
+        public ClipboardItem() { }
+
+        public ClipboardItem(string content, ContentType type)
+        {
+            Content = content;
+            Type = type;
+            Timestamp = DateTime.Now;
+        }
+
+    }
+}
---
+++ SmartClipboard/ViewModels/MainViewModel.cs
@@ -0,0 +1,60 @@
+﻿using SmartClipboard.Models;
+using SmartClipboard.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SmartClipboard.ViewModels
+{
+    internal class MainViewModel: INotifyPropertyChanged
+    {
+        private readonly DatabaseService _dbService = new();
+        private string _lastText = string.Empty;
+
+        public ObservableCollection<ClipboardItem> ClipboardItems { get; } = new();
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public MainViewModel()
+        {
+            _dbService = new DatabaseService();
+            LoadData();
+        }
+
+        public void SaveClipboardText(string text)
+        {
+            if (string.IsNullOrWhiteSpace(text) || text == _lastText)
+                return;
+
+            _lastText = text;
+
+            var item = new ClipboardItem
+            {
+                Content = text,
+                Timestamp = DateTime.Now,
+                Type = ClassificationService.Classify(text)
+            };
+
+            _dbService.InsertClipboardItem(item);
+            ClipboardItems.Insert(0, item);
+        }
+
+        public void LoadData()
+        {
+            var items = _dbService.GetAllItems();
+            ClipboardItems.Clear();
+            foreach (var item in items)
+                ClipboardItems.Add(item);
+        }
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
